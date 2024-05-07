4e903fc1865724adc8993a487eb5fe722a027533
cemarso <150005348+cemarso@users.noreply.github.com>
2024-05-07 14:33:07 +0200

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,13 +1,11 @@
 def FizzBuzz(num):
     for number in range(1, num+1):
-        # print(number)
-
-        if number % 3 == 0 and number % 5 != 0:
-            print('Fizz')
-        elif number % 5 == 0 and number % 3 != 0:
-            print('Buzz')
-        elif number % 3 == 0 and number % 5 == 0:
+        if number % 3 == 0 and number % 5 == 0:
             print('FizzBuzz')
+        elif number % 3 == 0:
+            print('Fizz')
+        elif number % 5 == 0:
+            print('Buzz')
         else:
             print(number)
 
