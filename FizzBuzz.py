aac1b7c1da0309142098f1d2127d8e2ddec2cd1e
cemarso <150005348+cemarso@users.noreply.github.com>
2024-05-07 13:37:07 +0200

    Add files via upload

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+def FizzBuzz(num):
+    for number in range(1, num+1):
+        # print(number)
+
+        if number % 3 == 0 and number % 5 != 0:
+            print('Fizz')
+        elif number % 5 == 0 and number % 3 != 0:
+            print('Buzz')
+        elif number % 3 == 0 and number % 5 == 0:
+            print('FizzBuzz')
+        else:
+            print(number)
+
+
+FizzBuzz(100)
